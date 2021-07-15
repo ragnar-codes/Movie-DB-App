713d5ed2c50755971e892eca453896de7327c63b
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2021-07-15 19:04:30 +0100

    add home.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //components
 import Header from './Components/Header/Header';
+import Home from './Components/Home';
 //styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -8,6 +9,7 @@ function App() {
     return (
         <div className="App">
             <Header />
+            <Home />
             <GlobalStyle />
 
         </div>
